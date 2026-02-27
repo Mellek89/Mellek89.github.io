ab85a7291f5f7a0811e09287b176811eb1b438ec
mellek89 <melaniekoester89@gmail.com>
2026-02-27 08:56:11 -0500

    menu pimpen

---
+++ Honig/js/script.js
@@ -0,0 +1,10 @@
+
+
+const menuitem = document.getElementById("tspan148");
+
+menuitem.addEventListener("click", (e) => {
+
+    console.log( e +"wurde geklickt");
+     window.location.href = "productos.html";
+
+});
